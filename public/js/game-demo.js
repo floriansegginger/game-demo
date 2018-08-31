7a813412d18a54329ab40d30f4c8b24399df9645
GermainVasto <42832269+GermainVasto@users.noreply.github.com>
2018-08-31 16:22:56 +0200

    step 5&6 completed
    Not done properly

--- public/js/game-demo.js
+++ public/js/game-demo.js
@@ -1,37 +1,38 @@
 var game = new Phaser.Game(1280, 720, Phaser.CANVAS, 'game-demo', { preload: preload, create: create });
 
 function preload() {
-
-    //  You can fill the preloader with as many assets as your game requires
-
-    //  Here we are loading an image. The first parameter is the unique
-    //  string by which we'll identify the image later in our code.
-
-    //  The second parameter is the URL of the image (relative)
     game.load.image('background', '/images/background.jpg');
     game.load.image('ball', '/images/ball.png');
+    game.load.image('ball2', '/images/ball.png');
 }
 
 var ball;
 
 function create() {
-    //  This creates a simple sprite that is using our loaded image and
-    //  displays it on-screen
     var backgroundImage = game.add.sprite(0, 0, 'background');
+    backgroundImage.inputEnabled = true;
 
-    var ball = game.add.sprite(640, 360, 'ball');
+    var ball = game.add.sprite(680, 360, 'ball');
+    var ball2 = game.add.sprite(600, 360, 'ball2');
     ball.anchor.setTo(0.5, 0.5);
-
-
+    ball2.anchor.setTo(0.5, 0.5);
     game.physics.enable(ball, Phaser.Physics.ARCADE);
+    game.physics.enable(ball2, Phaser.Physics.ARCADE);
 
-    ball.body.velocity.x = 75;
+    ball.body.velocity.x = 100;
     ball.body.velocity.y = 100;
+    ball2.body.velocity.x = -100;
+    ball2.body.velocity.y = -100;
 
     ball.update = ballUpdate;
+    ball2.update = ballUpdate;
 
     ball.inputEnabled = true;
+    ball2.inputEnabled = true;
     ball.events.onInputDown.add(ballClick, ball);
+    ball2.events.onInputDown.add(ballClick, ball2);
+
+    backgroundImage.events.onInputDown.add(backgroundClick, backgroundImage, ball)
 }
 
 function ballUpdate() {
@@ -63,6 +64,24 @@ function render() {
     // game.debug.text('deltaZ: ' + sprite.body.deltaZ(), 32, 296);
 }
 
+function backgroundClick() {
+	var mouseClicX = game.input.mousePointer.x
+    var mouseClicY = game.input.mousePointer.y
+
+    var ball = game.add.sprite(mouseClicX, mouseClicY, 'ball');
+    ball.anchor.setTo(0.5, 0.5);
+    game.physics.enable(ball, Phaser.Physics.ARCADE);
+
+    ball.body.velocity.x = 100;
+    ball.body.velocity.y = 100;
+
+    ball.update = ballUpdate;
+
+    ball.inputEnabled = true;
+    ball.events.onInputDown.add(ballClick, ball);
+    
+}
+
 function ballClick() {
     var realCenterBallX = this.body.position.x + 23;
     var realCenterBallY = this.body.position.y + 21.5;
@@ -73,7 +92,7 @@ function ballClick() {
     var mouseClicY = game.input.mousePointer.y - realCenterBallY
 
     //the factor is used to maintain the same speed no matter where the user click on the ball
-    var factor = 150 / Math.sqrt((mouseClicX * mouseClicX) + (mouseClicY * mouseClicY))
+    var factor = Math.sqrt(20000) / Math.sqrt((mouseClicX * mouseClicX) + (mouseClicY * mouseClicY))
 
     //the ball go in the opposite direction of the click
     this.body.velocity.x = - mouseClicX * factor
